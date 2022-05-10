31b7e5625788fda8785d03d8554e7cb12cdc8d8f
RK-Shandilya <89962501+RK-Shandilya@users.noreply.github.com>
2022-05-10 12:22:03 +0530

    Create Leetcode-349.cpp

---
+++ Leetcode-349.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        int i=0;
+        int j=0;
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        vector<int>v;
+        while(i<nums1.size() && j<nums2.size()){
+            if(nums1[i]==nums2[j]){
+                if(v.empty()){
+                    v.push_back(nums1[i]);
+                }
+                if(v.back()!=nums1[i]){
+                    v.push_back(nums1[i]);
+                }
+                i++;
+                j++;
+            }
+            else if(nums1[i]<nums2[j]){
+                i++;
+            }
+            else{
+                j++;
+            }
+        }
+        return v;
+    }
+};
